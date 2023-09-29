03c6e8b79f6198b43a3946fa4089e17135097453
Manisha Kundnani <90680330+Manishak798@users.noreply.github.com>
2023-09-29 08:21:50 +0530

    Add files via upload

---
+++ JavaScript-Advance/new/arrymethods.js
@@ -0,0 +1,11 @@
+let myarry=["fan","tv","cherry",15,5,null,true];
+
+console.log(myarry.length);
+myarry.push("mango");
+console.log(myarry);
+let newarry=["watermelon",false];
+myarry.concat(newarry+myarry);
+console.log(newarry);
+/*.pop(removes from last)
+.shift(removes from start)
+.unshift(add element at start)*/
---
+++ JavaScript-Advance/new/forloop.js
@@ -0,0 +1,13 @@
+arr = [1,2,3,4,5,6,7,8];
+// for(var i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+/*for each loop in array*/
+arr.forEach(element => {
+    console.log(arr);
+});
+/*Arrow functions were introduced in ES6.
+
+Arrow functions allow us to write shorter function syntax:
+
+let myFunction = (a, b) => a * b;*/
