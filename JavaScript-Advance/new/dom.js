cf57a28026045dd97d8d5731d9f1e002a4508345
Manisha Kundnani <90680330+Manishak798@users.noreply.github.com>
2023-09-29 19:35:52 +0530

    Add files via upload

---
+++ JavaScript-Advance/new/dom.js
@@ -0,0 +1,9 @@
+document.getElementById('click').style.background=("red");
+let element=document.getElementsByClassName("para");
+for(var i=0;i<element.length;i++){
+element[i].style.color="red";
+}/*whenever selecting whole string itrate through to change color of every letter*/
+let btn = document.getElementById('click');
+btn.style.color="lavender";
+let myqry= document.querySelector("h1");
+myqry.innerHTML="NEW CONTENT...";
